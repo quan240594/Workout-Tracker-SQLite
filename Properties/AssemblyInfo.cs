a3e849f168991fad127c0806a3ea248e1fd39674
quan240594 <64461691+quan240594@users.noreply.github.com>
2020-07-22 15:47:06 +0200

    Unable to deploy on virtual machines

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Workout-Tracker-SQLite")]
+[assembly: AssemblyTitle("Workout Tracker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Workout-Tracker-SQLite")]
+[assembly: AssemblyProduct("Workout Tracker")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
